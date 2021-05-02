7fafcdcf675459cdeb81f29ab553f60f1768539b
Enes Yilmaz <enesyilmaz60@hotmail.com>
2021-05-02 20:21:32 +0200

    mogodb connect delete new contact usw

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT || 3002
 const mongoose = require('mongoose')
+const formidable = require('formidable')
+const { v4: uuidv4 } = require('uuid');
 const contactItem = require('./models//contactModel')
 const app = express()
 const DBURI = process.env.DBURI
@@ -13,13 +15,20 @@ mongoose.connect(DBURI, { useNewUrlParser: true, useUnifiedTopology: true, useFi
   })
 })
 
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+// parse application/json
+app.use(express.json())
+
 app.use(express.static('public'))
+app.use(express.static('imgUpload'))
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   contactItem.find()
     .then(contacts => {
-      console.log(contacts);
+      // console.log(contacts);
       res.render('pages/index', { contacts: contacts });
     })
     .catch(err => console.log(err))
@@ -27,11 +36,45 @@ app.get('/', (req, res) => {
 })
 
 app.get('/new', (req, res) => {
-  const newPerson = new contactItem({
-    firstName: 'John',
-    lastName: 'Doe',
-    phoneNumber: '123-456-123',
-    favorite: false
-  }).save()
-  res.redirect('/')
+  res.render('pages/newContact')
+})
+
+
+app.post('/newContact',
+  (req, res, next) => {
+    const form = formidable({ multiples: true, uploadDir: './imgUpload', keepExtensions: true });
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      // console.log(`${files.profilPic.path.slice(9)} hier ist img`);
+      let new_person = new contactItem({
+        _id: uuidv4(),
+        profile_bild: `${files.profilPic.path.slice(9)}`,
+        firstName: fields.fname,
+        lastName: fields.lname,
+        phoneNumber: fields.telNumber,
+        favorite: false,
+        organisation: fields.organization,
+      }).save()
+      res.redirect('/')
+      // res.json({ fields, files });
+    });
+  });
+app.get('/details/:id', (req, res) => {
+  contactItem.findById(req.params.id)
+    .then(item => {
+      console.log(item);
+      res.render('pages/contactDetails', { contact: item })
+    })
+    .catch(err => console.log(err))
+})
+app.get('/delete/:id', (req, res) => {
+  contactItem.findByIdAndDelete(req.params.id)
+    .then(item => {
+      res.redirect('/')
+    })
+    .catch(err => console.log(err))
 })
--- models/contactModel.js
+++ models/contactModel.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const newContactItem = new Schema({
+  _id: String,
+  profile_bild: String,
   firstName: String,
   lastName: String,
   phoneNumber: String,
-  favorite: Boolean
-});
+  favorite: Boolean,
+  organisation: String
+}, { timestamps: true });
 
 const newContact = mongoose.model('contact', newContactItem)
 module.exports = newContact
